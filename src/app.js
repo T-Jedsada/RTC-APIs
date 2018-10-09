a42d8bc764e260be20771f6cc97cebe26441e28a
Jedsda Tiwongvorakul <jt@20scoops.net>
2018-10-09 16:50:12 +0700

    add other route

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ var boom = require('express-boom')
 var app = express()
 app.use(boom())
 var port = process.env.PORT || 3000
+var API_URL = 'http://chiangmaibackend.yusai.asia/smartapp/APIBUSNEW/'
 
 app.get('/', (_, res) => {
     res.send('RTC-APIs CNX')
 })
 
 app.get('/drivers', (_, res) => {
-    requestOldAPIs('http://chiangmaibackend.yusai.asia/smartapp/APIBUSNEW//APIThebus.php')
+    requestOldAPIs(`${API_URL}APIThebus.php`)
         .then(response => {
             res.send(response)
         })
@@ -20,7 +21,67 @@ app.get('/drivers', (_, res) => {
 })
 
 app.get('/buss', (_, res) => {
-    requestOldAPIs('http://chiangmaibackend.yusai.asia/smartapp/APIBUSNEW//APIbus.php')
+    requestOldAPIs(`${API_URL}APIbus.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/green', (_, res) => {
+    requestOldAPIs(`${API_URL}kku_green.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/purple', (_, res) => {
+    requestOldAPIs(`${API_URL}kku_purple.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/back', (_, res) => {
+    requestOldAPIs(`${API_URL}cm_back.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/bus_stop', (_, res) => {
+    requestOldAPIs(`${API_URL}APIbusstop.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/bus_line', (_, res) => {
+    requestOldAPIs(`${API_URL}APIbuspastLine.php`)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            res.boom.badRequest(err)
+        })
+})
+
+app.get('/bus_schedule', (_, res) => {
+    requestOldAPIs(`${API_URL}APIschedulebus.php`)
         .then(response => {
             res.send(response)
         })
